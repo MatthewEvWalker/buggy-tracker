1a7709e4a9fb33a11f53f603e37d55ce72c737bf
Matthew Walker <108114157+MatthewEvWalker@users.noreply.github.com>
2024-05-20 23:57:49 -0500

    Update index.js
    added the frontend link from vercel to the cors origin

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const PORT = 3001;
 app.use(express.json());
 app.use(cors(
     {
-        origin: [""],
+        origin: ["https://buggy-tracker-frontend.vercel.app/"],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true
     }
