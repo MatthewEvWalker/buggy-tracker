adcb2371a8fa191f0c8b7aff763013390a6efff6
Matthew Walker <108114157+MatthewEvWalker@users.noreply.github.com>
2024-05-21 00:19:35 -0500

    Update index.js
    commented out the hello app.get and deleted the /get in the app.get that has the mongo find function in it.

--- server/index.js
+++ server/index.js
@@ -15,9 +15,9 @@ app.use(cors(
     }
 ));
 
-app.get("/", (req, res) => {
-    res.json("hello");
-})
+// app.get("/", (req, res) => {
+//     res.json("hello");
+// })
 
 
 // use this one to get local host server
@@ -31,7 +31,7 @@ mongoose.connect('mongodb+srv://matthewevwalker:Foutin1965@buggy-data.ufnlatf.mo
 
 
 
-app.get("/get", async (req, res) => {
+app.get("/", async (req, res) => {
     try {
         const todos = await TodoModel.find({});
         res.json(todos);
